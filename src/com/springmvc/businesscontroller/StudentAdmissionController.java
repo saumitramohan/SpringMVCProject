3ae6d10448a1a8e7feda6c66663583aa038a358b
Saumitra Shukla <saumitra90@gmail.com>
2018-03-30 15:38:09 -0700

    Custom editor - Property editor

--- src/com/springmvc/businesscontroller/StudentAdmissionController.java
+++ src/com/springmvc/businesscontroller/StudentAdmissionController.java
@@ -1,5 +1,9 @@
 package com.springmvc.businesscontroller;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
@@ -15,9 +19,13 @@ import com.spring.businessObject.Student;
 public class StudentAdmissionController {
 	
 	// Basic example of data binding
+	// Spring uses property editor to internally bind the data
+	// Type conversion
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		binder.setDisallowedFields(new String[] {"studentMobile"});
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy---MM---dd");
+		binder.registerCustomEditor(Date.class, "DOB", new CustomDateEditor(dateFormat, false));
 	}
 	
 	@RequestMapping (value = "/admissionForm.html", method = RequestMethod.GET)
