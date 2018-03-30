df270cc503b3b0c6266f1ecdb740e87ff93d7353
Saumitra Shukla <saumitra90@gmail.com>
2018-03-29 20:54:36 -0700

    Using RequestMapping and Controller annotation

--- src/com/springmvc/businessobject/HelloController.java
+++ src/com/springmvc/businessobject/HelloController.java
@@ -3,13 +3,16 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-public class HelloController extends AbstractController {
+// Annotation based approach
+@Controller
+public class HelloController{
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@RequestMapping ("/welcome.html")
+	protected ModelAndView helloWorld() {
 		// TODO Auto-generated method stub
 		ModelAndView modelandview = new ModelAndView("HelloPage");
 		modelandview.addObject("welcomeMessage","Hi User, Welcome to SpringMVC application");
