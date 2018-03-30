d52c60d54f1ebce047b306c8625f12fc6807b34b
Saumitra Shukla <saumitra90@gmail.com>
2018-03-30 15:30:12 -0700

    Basic dataBinding for ignoring field

--- src/com/springmvc/businesscontroller/StudentAdmissionController.java
+++ src/com/springmvc/businesscontroller/StudentAdmissionController.java
@@ -2,16 +2,23 @@ package com.springmvc.businesscontroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.businessObject.Student;
 
 @Controller
 public class StudentAdmissionController {
+	
+	// Basic example of data binding
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.setDisallowedFields(new String[] {"studentMobile"});
+	}
 	
 	@RequestMapping (value = "/admissionForm.html", method = RequestMethod.GET)
 	public ModelAndView getAdmissionForm() {
