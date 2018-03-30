04aa5ff0737d52bbbbadf9995a67cd4de08a70f7
Saumitra Shukla <saumitra90@gmail.com>
2018-03-29 21:01:27 -0700

    MultiActionController

--- src/com/springmvc/businessobject/HelloController.java
+++ src/com/springmvc/businessobject/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 // Annotation based approach
 @Controller
+@RequestMapping ("/SpringWorld")
 public class HelloController{
 
 	@RequestMapping ("/welcome.html")
@@ -16,6 +17,14 @@ public class HelloController{
 		// TODO Auto-generated method stub
 		ModelAndView modelandview = new ModelAndView("HelloPage");
 		modelandview.addObject("welcomeMessage","Hi User, Welcome to SpringMVC application");
+		return modelandview;
+	}
+	
+	@RequestMapping ("/hi.html")
+	protected ModelAndView hi() {
+		// TODO Auto-generated method stub
+		ModelAndView modelandview = new ModelAndView("HelloPage");
+		modelandview.addObject("welcomeMessage","Hi Saumitra, Welcome to SpringMVC application");
 		return modelandview;
 	}
 
