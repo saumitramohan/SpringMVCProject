1785e589b43d55c592e88c48f47ad0bb3119499f
Saumitra Shukla <saumitra90@gmail.com>
2018-03-31 16:32:28 -0700

    Response status exception

--- src/com/springmvc/globalexception/GlobalExceptionHandlerMethods.java
+++ src/com/springmvc/globalexception/GlobalExceptionHandlerMethods.java
@@ -1,17 +1,21 @@
 package com.springmvc.globalexception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandlerMethods {
 	
+	@ResponseStatus (value= HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler (value = NullPointerException.class)
 	public String handleNullPointerException (Exception e) {
 		System.out.println("NPE");
 		return "NullPointerException";
 	}
 	
+	@ResponseStatus (value= HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler (value = Exception.class)
 	public String handleGenericException (Exception e) {
 		System.out.println("Generic Exception");
